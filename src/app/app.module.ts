c904cf1e490d13928519613d9a4854e4f0ed522d
tdebenne41@gmail.com <Thomas Debenne>
2022-12-14 10:06:59 +0100

    Merge develop

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,16 +7,27 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CanvasComponent } from './components/canvas/canvas.component';
 import SvgElementsService from './services/svg-elements.service';
 import { StorageService } from './services/storage.service';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ToolsSidebarComponent } from './components/tools-sidebar/tools-sidebar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent,
-    CanvasComponent
+    CanvasComponent,
+    ToolsSidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatIconModule,
+    HttpClientModule
+
   ],
   providers: [
     SvgElementsService,
