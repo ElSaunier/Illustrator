ee742f7de0a63b5913ddf5342e413569749617f9
Arthur LEJAY <arthur.lejay@etu.unistra.fr>
2023-02-01 11:23:21 +0100

    feat: add coordinates as well as getters and setters

--- src/app/lib/actions/action.class.ts
+++ src/app/lib/actions/action.class.ts
@@ -1,9 +1,40 @@
 import { Tool } from '@lib/tools/tool.abstract';
+import { Shape } from '@lib/interfaces/shape.interface';
 
 export class Action {
-  protected isPending = true;
-  protected shapes = [];
+  constructor(protected x: number, protected y: number, protected shapes: Shape[], protected toolType: typeof Tool, protected isPending = true) {
+  }
 
-  constructor(protected _toolType: typeof Tool) {
+  getPending(): boolean {
+    return this.isPending;
+  }
+
+  setPending(isPending: boolean) {
+    this.isPending = isPending;
+  }
+
+  getCoordinates(): { x: number, y: number } {
+    return { x: this.x, y: this.y };
+  }
+
+  setCoordinates(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+
+  getShapes(): Shape[] {
+    return this.shapes;
+  }
+
+  setShapes(shapes: Shape[]) {
+    this.shapes = shapes;
+  }
+
+  getToolType(): typeof Tool {
+    return this.toolType;
+  }
+
+  setToolType(toolType: typeof Tool) {
+    this.toolType = toolType;
   }
 }
