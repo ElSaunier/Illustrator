060c54a5cca69056a0e334c4ff7f6c918515e75b
m.fessler <mathieu.fessler@etu.unistra.fr>
2023-04-01 17:44:35 +0200

    Simplification of the tool rectangle

--- src/app/lib/tools/rectangle-tool.class.ts
+++ src/app/lib/tools/rectangle-tool.class.ts
@@ -116,6 +116,10 @@ export class RectangleTool extends Tool {
     const coord1 = lastAction.getCoordinates();
     const coord2 = beforeLastAction.getCoordinates();
 
+    // Remove the last 2 actions to draw the rectangle 
+    stack.undo();
+    stack.undo();
+
     const newAction = new Action(
       0,
       0,
